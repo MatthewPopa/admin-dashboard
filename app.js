bad5e357b558c4bfa50e0a9e3f5e1f34cb768a0c
MatthewPopa <matthewtpopa@gmail.com>
2024-01-22 00:20:17 -0500

    Dark/Light mode

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const navButton = document.querySelector('.nav-menu-button');
+const navBtn = document.querySelector('.nav-menu-button');
 const overlayToggle = document.querySelector('.toggle-overlay');
+const themeBtn = document.querySelector('.theme-select');
 
 document.addEventListener('click', (e) => {
-    if(e.target == navButton || e.target.parentNode == navButton || e.target.parentNode.parentNode == navButton) {
+    if (e.target == navBtn || e.target.parentNode == navBtn || e.target.parentNode.parentNode == navBtn) {
         document.querySelector('aside').classList.add('visible');
         document.querySelector('.toggle-overlay').classList.add('enabled');
     }
-    if(e.target == overlayToggle) {
+    if (e.target == overlayToggle) {
         document.querySelector('aside').classList.remove('visible');
         document.querySelector('.toggle-overlay').classList.remove('enabled');
     }
     console.log(e.target);
-})
+});
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+document.addEventListener('click', () => {
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+    if (prefersDarkScheme.matches) {
+        var theme = document.body.classList.contains("light-theme")
+            ? "light"
+            : "dark";
+    } else {
+        var theme = document.body.classList.contains("dark-theme")
+            ? "dark"
+            : "light";
+    }
+    localStorage.setItem("theme", theme);
+});
