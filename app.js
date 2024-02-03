ef0f62c8ecc0e2b2cf5ea287ad082cb2e3cc8768
MatthewPopa <matthewtpopa@gmail.com>
2024-02-03 02:51:59 -0500

    Add tooltip functionality

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const themeMenu = document.querySelector('#theme-select');
 const headerMenu = document.querySelector('.header-menu');
 let activeMenu = false;
 const progressBars = document.querySelectorAll('.progress');
+const tooltipInfo = document.querySelectorAll('.tooltip');
+const tooltip = document.querySelector('.tooltip-modal');
 
 function updateProgress() {
     progressBars.forEach((element) => {
@@ -13,7 +15,56 @@ function updateProgress() {
     })
 };
 
+// function populateTooltip() {
+//     tooltipInfo.forEach((element) => {
+//         element.addEventListener('mouseover', (e) => {
+//             let header = element.dataset.header;
+//             let body = element.dataset.body;
+//             tooltip.querySelector('.tooltip-header').textContent = header;
+//             tooltip.querySelector('.tooltip-body').textContent = body;
+//         });
+//     });
+// }
+
+document.addEventListener('scroll', fadeTooltip);
+
+function populateTooltip() {
+    tooltipInfo.forEach((element) => {
+        element.addEventListener('mouseover', moveTooltip)
+    });
+    tooltipInfo.forEach((element) => {
+        element.addEventListener('mouseleave', fadeTooltip)
+    });
+    tooltipInfo.forEach((element) => {
+        element.addEventListener('mouseover', (e) => {
+            let header = element.dataset.header;
+            let body = element.dataset.body;
+            tooltip.querySelector('.tooltip-header').textContent = header;
+            tooltip.querySelector('.tooltip-body').textContent = body;
+        });
+    });
+}
+
+function moveTooltip(event) {
+    tooltip.style.opacity = '1';
+    tooltip.style.visibility = 'visible';
+    let x = event.clientX;
+    let y = event.clientY;
+    tooltip.style.top = y - tooltip.offsetHeight - 10 + 'px';
+    if ((x + tooltip.offsetWidth) > window.innerWidth) {
+        tooltip.style.left = x - tooltip.offsetWidth + 'px';
+        return;
+    }
+    tooltip.style.left =  x + 10 + 'px';
+}
+
+function fadeTooltip() {
+    tooltip.style.opacity = '0';
+    tooltip.style.visibility = 'hidden';
+}
+
 updateProgress();
+populateTooltip();
 
 document.addEventListener('click', (e) => {
     if(activeMenu) {
